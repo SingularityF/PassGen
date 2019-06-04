90e7b40e6dd130af83e4132358d56ce78f38fb04
SingularityF <mailtosingularityf@gmail.com>
2019-06-04 16:29:59 -0400

    Rename repo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PassGen'`, () => {
+  it(`should have as title 'PassOGen'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('PassGen');
+    expect(app.title).toEqual('PassOGen');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to PassGen!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to PassOGen!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PassGen';
+  title = 'PassOGen';
   textInputType = "password"
   outputInputType = "password"
   storedInputText = localStorage.getItem("inputText")
